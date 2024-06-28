b83b92d2682664d5ff87d7c917ca1e254daa0994
Hexi1997 <18715156450@163.com>
2024-06-28 16:54:23 +0800

    feat: add ccc wallet ui

--- src/main.tsx
+++ src/main.tsx
@@ -2,9 +2,12 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.tsx'
 import './index.css'
+import * as ccc from '@ckb-ccc/connector-react'
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <App />
+    <ccc.Provider>
+      <App />
+    </ccc.Provider>
   </React.StrictMode>,
 )
